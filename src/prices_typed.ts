b3f9310af5641db2e0ec5fa50254781d275e0e81
D3lux3 <dani.haapaniemi@hotmail.com>
2024-01-31 21:12:46 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -88,7 +88,7 @@ function createApp(database: Database) {
       let holiday = new Date(row.holiday);
       const holiday2 = parseDate2(row.holiday);
       if (
-        date && date2 && holiday2 && date2.equals(holiday2)
+        date2 && holiday2 && date2.equals(holiday2)
       ) {
         return true;
       }
